70e255c958cc100a6940e22a4f3a4bc46260e478
Jeremiah Haynes <jhaynes@jhaynes-m02.ad.savagebeast.com>
2019-08-12 04:25:27 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,14 +2,31 @@
 const cards = ["queen", "queen", "king", "king"]
 const cardsInPlay = []
 
-var cardOne = [0];
-cardsInPlay.push(cardOne); {console.log("User flipped queen")}
 
-var cardTwo = [2]
-cardsInPlay.push(cardTwo); {console.log("User flipped king")}
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+};
 
-if (cardsInPlay.length === 2 && (cardsInPlay[0]) === (cardsInPlay[1])) {
+function flipCard(cardID) {
+	if (cardsInPlay.length === 2 && (cardsInPlay[0]) === (cardsInPlay[1])) {
 	alert("You found a match!");
 } else { 
-	alert("Sorry, try again.");
+	alert("Sorry, try again."); 
+
+console.log("User flipped" + cards[cardID]);
+
+
 }
+};
+
+flipCard(0); 
+flipCard(2);
+checkForMatch();
+
+var cardID = []
+cardsInPlay.push(cards[cardID]);
+
