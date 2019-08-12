6f69c9d3f51dc9599a2b9b6b3a8dc55474a3e032
Jeremiah Haynes <jhaynes@jhaynes-m02.ad.savagebeast.com>
2019-08-12 13:24:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -19,14 +19,13 @@ var cards = [
 	rank: 'king',
 	suit: 'diamonds',
 	cardImage: "images/king-of-diamonds.png",
-},
+}
 ];
 
 var cardsInPlay = [];
 
 
 var checkForMatch = function() {
-	cardsInPlay.push(cards[cardID].rank);
 	if (cardsInPlay[0] === cardsInPlay[1]) {
   		alert("You found a match!");
 	} else {
@@ -34,20 +33,32 @@ var checkForMatch = function() {
 	}
 };
 
-var flipCard = function(cardID) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
-	};
+	}
 
-console.log("User flipped" + cards[cardID].rank);
-console.log(cards[cardID].cardImage);
-console.log(cards[cardID].suit);
+console.log("User flipped" + cards[cardId].rank);
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
 
 
 };
 
-flipCard(0); 
-flipCard(2);
+var createBoard = function (){
+	for (var i = 0; i < cards.length; i++) {
+	var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+}
+};
+
+createBoard();
 
 
 
